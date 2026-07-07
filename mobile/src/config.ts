d5f446c6397d9bd1f473c90ff71bbe126268a0d6
Alf33140 <alf2kdavy@yahoo.com>
2026-07-07 16:04:37 +0200

    start projet apk mobile

---
+++ mobile/src/config.ts
@@ -0,0 +1,14 @@
+//=================================================================
+// IMPORTANT : remplace l IP par ton IP ci-dessous par l IP locale de ta machine
+// (pas localhost -  Expo s execute sur ton telephone et pas ton PC)
+// 
+// pour trouver ton IP
+// windows : ipconfig -> " adresse ipv4" de ta carte wifi
+// ex: 192.168.42
+//
+// ton backend ecoute sur le port 5000
+
+
+export const API_URL = "http://192.168.1.137:5000/api" // adresse IP v4 WIFI Maison
+
+// export const API_URL = "http://xxx.xxx.x.xxx:5000/api" // dresse IP v4 AFPA a changer
